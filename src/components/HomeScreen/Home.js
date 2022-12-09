85db4f74ab2d5564fc249a022bcda2b3bc57d59f
bhoomikajethwani <bhoomsdj@gmail.com>
2022-12-09 16:51:41 -0700

    House card for home

--- src/components/HomeScreen/Home.js
+++ src/components/HomeScreen/Home.js
@@ -1,13 +1,20 @@
 import bg from "../../imgs/bg.jpeg"
 import SearchBar from "../SearchBar/SearchBar";
 import './HomeScreen.css'
+import HouseCard from "../House/HouseCard.js"
+import React from "react"
+import { Container } from "react-bootstrap"
 
 function Home() {
     return(
-        <div>
-            <img src={bg} className="search-background-img"/>
-            <SearchBar></SearchBar>
-        </div>
+            <div>
+                <img src={bg} className="search-background-img"/>
+                <SearchBar></SearchBar>
+                <div>
+                    <HouseCard></HouseCard>
+                </div>
+            </div>
+        
     )
 }
 
