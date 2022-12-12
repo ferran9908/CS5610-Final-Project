44b9f1f19ec8ad72fc380585c4478d71607edba3
Ferran <ferran.9908@gmail.com>
2022-12-11 20:45:45 -0800

    Implement MapContainer

--- src/components/Map/MapContainer.js
+++ src/components/Map/MapContainer.js
@@ -1,10 +1,11 @@
-import React, {useEffect, useState} from 'react'
-import {GoogleMap, LoadScript, MarkerF, useJsApiLoader} from '@react-google-maps/api';
-import {useLocation, useParams} from "react-router-dom";
+import React, { useEffect, useState } from 'react'
+import { GoogleMap, LoadScript, MarkerF } from '@react-google-maps/api';
+import { useParams } from "react-router-dom";
 import axios from "axios";
 import './MapContainer.css'
-import {useDispatch} from "react-redux";
-import {getHousesByZip} from "../../store/slices/houseSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { getHousesByZip } from "../../store/slices/houseSlice";
+import HouseCard from '../House/HouseCard';
 
 const containerStyle = {
     width: '500px',
@@ -14,11 +15,13 @@ const containerStyle = {
 const BASE_URL = `https://maps.googleapis.com/maps/api/geocode/json?address=`
 const API_KEY = "AIzaSyBK-xckagY7ua5F0qsjtzDVnxL0-63BquU"
 
-const Map =  () => {
-    const {zip} = useParams()
+const Map = () => {
+    const { zip } = useParams()
     const [lat, setLat] = useState(0)
     const [lng, setLng] = useState(0)
     const dispatch = useDispatch()
+
+    const housesByZip = useSelector(state => state.houses.housesByZip)
 
     useEffect(() => {
         async function getLatLng() {
@@ -26,18 +29,29 @@ const Map =  () => {
             setLat(locationMap.data.results[0].geometry.location['lat'])
             setLng(locationMap.data.results[0].geometry.location['lng'])
         }
-        dispatch(getHousesByZip({zip}))
+        dispatch(getHousesByZip({ zip }))
         getLatLng()
-    },[])
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
 
     return (
         <div className="map-container">
             <LoadScript googleMapsApiKey={API_KEY}>
                 <GoogleMap
-                    mapContainerStyle={containerStyle} center={{lat: lat, lng: lng}} zoom={15} >
-                    <MarkerF position={{lat: lat, lng: lng}}></MarkerF>
+                    mapContainerStyle={containerStyle} center={{ lat: lat, lng: lng }} zoom={15} >
+                    <MarkerF position={{ lat: lat, lng: lng }}></MarkerF>
                 </GoogleMap>
             </LoadScript>
+            <div>
+                {housesByZip && housesByZip.map(house => {
+                    return <HouseCard name={house.name}
+                        streetAddress={house.streetAddress}
+                        price={house.price}
+                        description={house.description}
+                        images={house.images} />
+                })}
+
+            </div>
         </div>
     )
 }
