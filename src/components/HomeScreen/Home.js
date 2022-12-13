872637a7d1f5aa237a8ebd13885f272fd3b4d9a2
Ferran <ferran.9908@gmail.com>
2022-12-12 22:37:39 -0800

    Fix Bug

--- src/components/HomeScreen/Home.js
+++ src/components/HomeScreen/Home.js
@@ -2,11 +2,18 @@
 import SearchBar from "../SearchBar/SearchBar";
 import './HomeScreen.css'
 import AllHouses from "../House/AllHouses.js"
-import React from "react"
+import React, { useEffect } from "react"
+import { useDispatch } from "react-redux";
+import { findAllHouses } from "../../store/slices/houseSlice";
 // import { Container } from "react-bootstrap"
 
 
 function Home() {
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(findAllHouses())
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
     return (
         <div className>
             <img src="https://images.unsplash.com/photo-1632120669818-ed5498030e32?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2831&q=80"
